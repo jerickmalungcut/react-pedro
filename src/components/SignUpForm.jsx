3713bec2005d126a4c0cc7ad0f950cd74f7abb7c
jerickmalungcut <jerickmalungcut@yahoo.com>
2026-03-11 23:50:13 +0800

    3rd commit

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const SignUpForm = () => {
+  const { register, handleSubmit } = useForm();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
     console.log("Email:", email);
     console.log("Password:", password);
@@ -14,15 +16,26 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="Email">
           Email:
           <input
             type="email"
             placeholder="Enter your email"
+            {...register("email", { required: true })}
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
+        <label htmlFor="Password">
+          Password:
+          <input
+            type="password"
+            placeholder="Enter your password"
+            {...register("password", { required: true })}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Sign up</button>
       </form>
     </div>
   );
